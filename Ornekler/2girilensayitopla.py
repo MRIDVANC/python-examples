ef38c434cc91a50a4a8067afaa7c72f289498142
RIDVAN <24781973+MRIDVANC@users.noreply.github.com>
2022-03-19 22:52:04 +0300

    Add files via upload

---
+++ Ornekler/2girilensayitopla.py
@@ -0,0 +1,10 @@
+# Bu program Girilen 2 sayıyı toplar
+
+sayi1 = input('Bir sayı giriniz: ')
+sayi2 = input('2. bir sayı daha rica ediyoruz: ')
+
+# Rakamları Topla
+toplam = float(sayi1) + float(sayi2)
+
+# Toplamı Göster
+print('1. Sayı  {0} ve 2. sayı  {1} Toplamları = {2}'.format(sayi1, sayi2, toplam))
