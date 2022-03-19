485d2dfa8ddcff7a3934e3d8986e9bebd2b24d36
RIDVAN <24781973+MRIDVANC@users.noreply.github.com>
2022-03-19 22:49:20 +0300

    Add files via upload

---
+++ Ornekler/2sayitopla.py
@@ -0,0 +1,10 @@
+# Bu program 2 sayıyı toplar
+
+sayi1 = 6 # NEDEN 06 OLMUYOR Kİ :)
+sayi2 = 51 
+
+# Rakamları Topla
+toplam = sayi1 + sayi2
+
+# Toplamı Göster
+print('1. Sayı  {0} ve 2. sayı  {1} Toplamları = {2}'.format(sayi1, sayi2, toplam))
